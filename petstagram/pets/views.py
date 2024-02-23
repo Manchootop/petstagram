51670cce4f100079d8c4020d9e61b7bfa86bccaf
Mariyan Georgiev <mariqn5000@gmail.com>
2024-02-23 23:00:05 +0200

    configured templates/urls and put placeholder views

---
+++ petstagram/pets/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+def create_pet(request):
+    context = {}
+
+    return render()
+
+
+def details_pet(request, pk):
+    pass
+
+
+def delete_pet(request, pk):
+    pass
+
+
+def edit_pet(request, pk):
+    pass
