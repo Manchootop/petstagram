3650b571bb2deca120a4ecadd278fa4396e7c17e
Mariyan Georgiev <mariqn5000@gmail.com>
2024-03-20 23:53:24 +0200

    started converting fbv to cbv

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from . import views as views
 
 urlpatterns = [
-    path('add/', views.create_pet, name='create pet'),
-    path('', views.create_pet, name='create pet'),
+    path('add/', views.PetCreateView.as_view(), name='create pet'),
+    # path('', views.create_pet, name='create pet'),
     # path(include('<str:username>/pet/<slug:pet slug>/')),
         path('<str:username>/pet/<slug:pet_slug>/',
          include([
-             path('', views.details_pet, name='details pet'),
+             path('', views.PetDetailView.as_view(), name='details pet'),
              path('edit/', views.edit_pet, name='edit pet'),
              path('delete/', views.delete_pet, name='delete pet'),
                 ]),
--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy, reverse
+from django.views import generic as views
 
 from petstagram.pets.forms import PetCreateForm, PetEditForm, PetDeleteForm
 from petstagram.pets.models import Pet
 
 
-def create_pet(request):
-    pet_form = PetCreateForm(request.POST or None)
+# def create_pet(request):
+#     pet_form = PetCreateForm(request.POST or None)
+#
+#     if request.method == 'POST':
+#         if pet_form.is_valid():
+#             created_pet = pet_form.save()
+#             return redirect('details pet', username='lubo', pet_slug=created_pet.slug)
+#     context = {
+#         'pet_form': pet_form,
+#     }
+#
+#     return render(request, 'pets/create_pet.html', context)
 
-    if request.method == 'POST':
-        if pet_form.is_valid():
-            created_pet = pet_form.save()
-            return redirect('details pet', username='lubo', pet_slug=created_pet.slug)
-    context = {
-        'pet_form': pet_form,
-    }
+class PetCreateView(views.CreateView):
+    model = Pet
+    # fields = ['name', 'date_of_birth', 'personal_photo']
+    template_name = 'pets/create_pet.html'
+    form_class = PetCreateForm
 
-    return render(request, 'pets/create_pet.html', context)
+    def get_success_url(self):
+        return reverse('details pet', kwargs={
+            'username': 'lubo',
+            'pet_slug': self.object.slug
+        })
 
 
 def edit_pet(request, username, pet_slug):
@@ -40,10 +54,17 @@ def edit_pet(request, username, pet_slug):
 
 def details_pet(request, username, pet_slug):
     context = {
-        'pet': Pet.objects.get(slug=pet_slug)
+        'object': Pet.objects.get(slug=pet_slug)
     }
 
     return render(request, 'pets/details_pet.html', context)
+
+
+class PetDetailView(views.DetailView):
+    model = Pet     # or 'queryset'
+    template_name = 'pets/details_pet.html'
+    # slug_field = 'pet_slug'
+    slug_url_kwarg = 'pet_slug'
 
 
 def delete_pet(request, username, pet_slug):
